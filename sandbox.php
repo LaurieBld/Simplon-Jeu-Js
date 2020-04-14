fb1a46a710a56b17d2d17fdbfee1c1acd72370c6
LaurieBld <48795369+LaurieBld@users.noreply.github.com>
2020-04-14 11:11:48 +0200

    Create sandbox.php

---
+++ sandbox.php
@@ -0,0 +1,16 @@
+<?php require("header.php") ?>
+
+<main id="gameSelection">
+    <div class="questions">
+        <div id="toppart">
+            <div alt="redchart" id ="allGamesTitle" class="titleCenter"><em class="red">S</em>andbox</div>
+        </div>
+    </div>
+</main>
+
+<section if="codeSandbox">
+            <iframe src="https://codesandbox.io/s/code-sandbox-simplon-0unp0" frameborder="0" width="100%" height="600px"></iframe>
+        </section>
+
+
+<?php require("footer.php") ?>
